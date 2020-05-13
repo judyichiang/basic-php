46e01731fbd7c1cd45ccd3a4a90462e35927b0e6
Judy Chiang <jiyc1119@gmail.com>
2020-05-12 21:17:11 -0700

    index footer

--- index.php
+++ index.php
@@ -23,24 +23,4 @@
       </div>
 
     </div>
-
-  </div> <!-- end content -->
-
-  <div class="footer">
-
-    <div class="wrapper">
-
-      <ul>
-        <li><a href="">Twitter</a></li>
-        <li><a href="">Facebook</a></li>
-      </ul>
-
-      <p>&copy;<?php echo date("Y");?> Personal Media Library</p>
-
-    </div>
-
-  </div>
-
-</body>
-
-</html>
+<?php include("inc/footer.php");?>
